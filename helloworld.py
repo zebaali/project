75fcbc1e99c345994dd0e78634498a8bd607b5c9
lovkesh chopra <lovkesh@ezeiatech.in>
2019-11-17 19:09:41 +0530

    print statement

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,21 @@
-print("hello world")
+print("hello world",end="...........")
+print("hello world 2",1234,end="\t")
+print("hello world 2",1234)
+
+'''
+int x = 1
+String s1 = "a"
+System.out.print(s1+x)
+'''
+####x =1
+####s1 = "a"
+####print(s1+x)
+#-> this will throw exception as types are not implicitly converted in python
+
+#variable declaration jaisa kuch nahi hota yaha
+#type declaration bhi kuch nahi hota
+x = 1
+print(type(x))
+
+x = "zeba"
+print(type(x))
